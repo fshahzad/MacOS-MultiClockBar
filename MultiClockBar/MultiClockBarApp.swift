167d0c3fe22145d97f2aebce241b539729549734
Farrukh Shahzad <fshahzad@gmail.com>
2025-11-07 12:59:23 +0000

    Initial Commit

---
+++ MultiClockBar/MultiClockBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultiClockBarApp.swift
+//  MultiClockBar
+//
+//  Created by Farrukh Shahzad on 07/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct MultiClockBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
